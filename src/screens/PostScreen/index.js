eaeb4e810ae07a64fd318cfa192f706a73a9787f
evanvvav <ivankolesov.fi@gmail.com>
2022-04-24 13:45:24 +0300

    Before fixes

--- src/screens/PostScreen/index.js
+++ src/screens/PostScreen/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import React from 'react';
-import { View, FlatList, ActivityIndicator  } from 'react-native';
+import { View, ActivityIndicator  } from 'react-native';
 import DetailedPost from "../../components/DetailedPost";
 import { useRoute } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
@@ -12,7 +12,8 @@ export default function PostScreen() {
     const [places, setPlaces] = useState([false]);
     const [loading, setLoading] = useState(false);
 
-    const post = places.find(post.location_id === route.post.location_id);
+   const post = places[1];
+   //.find(place => place === route.params.id);
 
     useEffect(() => {
     getPlacesData()
@@ -20,20 +21,13 @@ export default function PostScreen() {
         //console.log(data);
         setPlaces(data);
         setLoading(true);
-        console.log(post.location_id);
+        console.log(route.params.location_id);
        })
     }, [])
 
     return (
         <View>
             {loading ? 
-
-            /* <FlatList
-                data={places}
-                renderItem={({item, index}) => <Post post={item} />}
-                keyExtractor={(item, index) => index.toString()}
-            
-            /> */
 
             <DetailedPost post={post}
             /> : <ActivityIndicator size="large" color="#C996CC" />
